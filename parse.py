10d26ad004204f56d6b1038891cd33960eeb8ecb
Max Bothe <mathebox@gmail.com>
2024-04-05 20:30:00 +0200

    Lint code

--- parse.py
+++ parse.py
@@ -3,8 +3,9 @@ from pathlib import Path
 import requests
 import xml.etree.ElementTree as ET
 
-FEED_URL = 'https://raw.githubusercontent.com/mathebox/radioeins-podcast-split/feeds/radioeins-gut-und-boerse.xml'
-COVER_ART_URL = 'https://raw.githubusercontent.com/mathebox/radioeins-podcast-split/feeds/covers/radioeins-gut-und-boerse.png'
+FEED_URL = 'https://raw.githubusercontent.com/mathebox/radioeins-podcast-split/feeds/radioeins-gut-und-boerse.xml'  # noqa: E501
+COVER_ART_URL = 'https://raw.githubusercontent.com/mathebox/radioeins-podcast-split/feeds/covers/radioeins-gut-und-boerse.png'  # noqa: E501
+
 
 def is_interesting(item):
     if 'Gut & Börse' in item.find('title').text:
@@ -16,12 +17,13 @@ def is_interesting(item):
 
     return False
 
+
 def parse():
     url = 'https://www.radioeins.de/export/updates.xml/feed=podcast.xml'
     r = requests.get(url)
     root = ET.fromstring(r.text)
 
-    # Find and remove all items (podcast episodes) that don't have the title Gut & Börse'
+    # Find and remove all items (podcast episodes) that are not 'interesting'
     for item in root[0].findall('item'):
         if not is_interesting(item):
             root[0].remove(item)
@@ -37,12 +39,13 @@ def parse():
             href_tag.set('href', FEED_URL)
 
     # Remove empty lines
-    xml = os.linesep.join([s for s in ET.tostring(root).decode("utf-8").splitlines() if s])
+    single_lines = ET.tostring(root).decode("utf-8").splitlines()
+    xml = os.linesep.join([line for line in single_lines if line])
 
     Path('generated-feeds').mkdir(parents=True, exist_ok=True)
     with open('generated-feeds/radioeins-gut-und-boerse.xml', 'w') as f:
         f.write(xml)
-        
+
 
 if __name__ == "__main__":
     parse()
